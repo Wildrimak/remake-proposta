164830e9ed14a329084e85707b45fb69b4b6e7d7
Wildrimak <finalwildrimak@gmail.com>
2021-09-09 08:49:50 -0300

    010-nao - Atualiza PropostaRepository para adicionar busca por documento

--- src/main/java/br/com/wildrimak/proposta/domain/repositories/PropostaRepository.java
+++ src/main/java/br/com/wildrimak/proposta/domain/repositories/PropostaRepository.java
@@ -1,9 +1,13 @@
 package br.com.wildrimak.proposta.domain.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import br.com.wildrimak.proposta.domain.models.Proposta;
 
 public interface PropostaRepository extends CrudRepository<Proposta, String> {
+
+    Optional<Proposta> findByDocumento(String documento);
 
 }
